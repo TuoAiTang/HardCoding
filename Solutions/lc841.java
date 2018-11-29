86a17b782e77249ddebf5af4c128f8b022515c08
tc <931182716@qq.com>
2018-11-30 01:53:32 +0800

    finish today

---
+++ Solutions/lc841.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class lc841 {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> visited = new HashSet<>();
+        dfs(rooms, 0, visited);
+        return visited.size() == rooms.size();
+    }
+
+    private void dfs(List<List<Integer>> rooms, int cur, Set<Integer> visited){
+    	if(visited.contains(cur)) return;
+    	visited.add(cur);
+    	for(int next : rooms.get(cur))
+    		dfs(rooms, next, visited);
+    }
+}
