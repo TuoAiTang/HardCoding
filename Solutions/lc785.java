ccf58c39a41960e43d9f92f4028bb4014ade5225
tc <931182716@qq.com>
2018-11-30 21:26:33 +0800

    p

--- Solutions/lc785.java
+++ Solutions/lc785.java
@@ -1,20 +1,8 @@
-//34ms beats 3.53%
+//9ms beats 19%
 import java.util.*;
 class lc785 {
     public boolean isBipartite(int[][] graph) {
-        //sparse graph -> construct by adjacency table
-        List<List<Integer>> g = new ArrayList<>();
-        for(int i = 0; i < graph.length; i++)
-        	g.add(new ArrayList<>());
-        for(int i = 0; i < graph.length; i++){
-        	for(int j : graph[i]){
-        		g.get(i).add(j);
-        		g.get(j).add(i);
-        	}
-        }
-
         int [] colors = new int[graph.length]; //0 -> unknown; 1 -> red; -1 -> blue;
-
         //bfs
         Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < graph.length; i++){
@@ -23,7 +11,7 @@ class lc785 {
             colors[i] = 1;
             while(!q.isEmpty()){
                 int cur = q.remove();
-                for(int next : g.get(cur)){
+                for(int next : graph[cur]){
                     if(colors[next] == colors[cur]) return false;
                     if(colors[next] != 0) continue;
                     colors[next] = -colors[cur];
