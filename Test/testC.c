fbaf0d4b836cf6336ab0f5cceb350501a3529640
tc <931182716@qq.com>
2019-06-27 19:55:39 +0800

    clean test.c

--- Test/testC.c
+++ Test/testC.c
@@ -3,27 +3,8 @@
 #include <math.h>
 #include <string.h>
 
-typedef struct {
-	int start;
-	int end;
-} Interval;
-
-
-void checkEndian(){
-	int a = 0x12345678;
-	char* c = (char*) &a;
-	if(*c == 0x78)
-		printf("小端\n");
-	else
-		printf("大端\n");
-}
 int main(){
-	// double d = 3.14;
-	// int* dp = (int*) &d;
-	// *(dp) = 1073741824;
-	// printf("%.14f\n", d);
-	// printf("I LOVE CODING VERY MUCH!!!!\n\n");
-	printf(20 % 30);
+
 	return 0;
 }
 
